cdd82588160f59bfe3bec75275774519b1748bce
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-08-21 05:30:30 +0100

    mobile responsive

--- js/script.js
+++ js/script.js
@@ -1,22 +1,36 @@
-/*==================== toggle icon navbar ====================*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
 
-/*==================== scroll sections active link ====================*/
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let height = section.offsetHeight;
+        let offset = section.offsetTop - 150;
+        let id = section.getAttribute('id');
 
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
 
-    /*==================== sticky navbar ====================*/
+    
+let header = document.querySelector("header");
 
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+header.classList.toggle("sticky", window.scrollY > 100);
+
+menuIcon.classList.remove('bx-x');
+navIcon.classList.remove('active');
+
 };
-
-
-/*==================== scroll reveal ====================*/
-
-
-/*==================== typed js ====================*/
